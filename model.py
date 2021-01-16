d7755f513aa2048f74d255aca90c8589e8d9c683
Andreas Schlapbach <schlpbch@gmail.com>
2021-01-16 15:48:48 +0100

    add __eq__

--- model.py
+++ model.py
@@ -19,10 +19,17 @@ class Dossier(BaseModel):
     id: str
     phone: str
 
+    def __eq__(self, other):
+        return self.id == other.id
+        
 
 class PlaceReservation(BaseModel):
     id: str
     state: PlaceReservationState
     creationDate: str
     dossier: Dossier
+
+    def __eq__(self, other):
+        return self.id == other.id
+
 
